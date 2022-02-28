140eea17050b909a1095df6d9aa44c883e7e9792
Davi Luna <davijluna@gmail.com>
2022-02-28 17:17:04 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const botao = document.querySelector('#btn');
+const botaoInput = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
 
 botao.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -9,3 +11,14 @@ botao.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+function validar(){
+  if(checkbox.checked){
+    botaoInput.disabled = false;
+  }else{
+    botaoInput.disabled = true; 
+  }
+
+};
+
+checkbox.addEventListener('click', validar);

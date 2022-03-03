f46d9afefc135af6f297df1fb23c635c8045e16a
Davi Luna <davijluna@gmail.com>
2022-03-02 23:18:35 -0300

    adicionado comentario no JS

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const senha = document.querySelector('#senha');
 const botao = document.querySelector('#btn');
 const botaoInput = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
+// esta function foi encinada pelo Matheus mentoria para ajudar a passar no teste lint
 botao.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
